72a44f461f86fce5b7cf40fcb24a46222842961f
Dhurgham_Fahem <drm.fhm@gmail.com>
2024-02-07 02:29:07 +0300

    add enum extensions

---
+++ src/everyextension/EnumExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace EveryExtension;
+
+public static class EnumExtensions
+{
+    public static string GetDescription(this Enum value)
+    {
+        var field = value.GetType().GetField(value.ToString())!;
+        var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute))!;
+        return attribute == null ? value.ToString() : attribute.Description;
+    }
+
+    public static bool HasFlag<TEnum>(this Enum value, TEnum flag) where TEnum : Enum
+    {
+        var flagValue = Convert.ToInt64(flag);
+        var enumValue = Convert.ToInt64(value);
+        return (enumValue & flagValue) == flagValue;
+    }
+
+    public static TAttribute GetEnumAttribute<TAttribute>(this Enum value) where TAttribute : Attribute
+    {
+        var fieldInfo = value.GetType().GetField(value.ToString());
+        return (TAttribute)Attribute.GetCustomAttribute(fieldInfo!, typeof(TAttribute))!;
+    }
+}
--- src/tests/UnitTest1.cs
+++ src/tests/UnitTest1.cs
@@ -8,5 +8,13 @@ public class UnitTest1
         var lst = new List<int>();
 
         object obj = new object();
+
+        type t = type.None;
+        t.get
+    }
+
+    enum type
+    {
+        None = 0,
     }
 }
