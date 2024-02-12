fdcc56437e738036f053009894ecf6c129b3b28c
Dhurgham <drm.fhm@gmail.com>
2024-02-12 13:02:02 +0300

    add QueryableExtensions

--- src/everyextension/LinqExtensions.cs
+++ src/everyextension/LinqExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace EveryExtension;
+﻿using System.Text;
+
+namespace EveryExtension;
 
 public static class LinqExtensions
 {
@@ -9,51 +11,47 @@ public static class LinqExtensions
     }
 
     public static IEnumerable<(T Item, int Index)> Index<T>(this IEnumerable<T> source)
-    {
-        return source.Select((item, index) => (item, index));
-    }
+        => source.Select((item, index) => (item, index));
 
     public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, bool condition, Func<T, bool> predicate)
-    {
-        return condition ? source.Where(predicate) : source;
-    }
+        => condition ? source.Where(predicate) : source;
 
     public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, bool condition, Func<T, bool> truePredicate, Func<T, bool> falsePredicate)
-    {
-        return condition ? source.Where(truePredicate) : source.Where(falsePredicate);
-    }
+        => condition ? source.Where(truePredicate) : source.Where(falsePredicate);
 
     public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, Func<T, int, bool> condition, Func<T, bool> predicate)
-    {
-        return source.Where((item, index) => condition(item, index)).Where(predicate);
-    }
+        => source.Where((item, index) => condition(item, index)).Where(predicate);
 
+    public static IEnumerable<TResult> SelectIf<T, TResult>(this IEnumerable<T> query, bool condition, Func<T, TResult> selector)
+       => condition ? query.Select(selector) : query.Cast<TResult>();
+
+    public static IEnumerable<TResult> SelectIf<T, TResult>(this IEnumerable<T> query, bool condition, Func<T, TResult> trueSelector, Func<T, TResult> falseSelector)
+        => condition ? query.Select(trueSelector) : query.Select(falseSelector);
+
+    public static bool AnyIf<T>(this IEnumerable<T> query, bool condition, Func<T, bool> predicate)
+        => condition ? query.Any(predicate) : true;
+
+    public static bool AnyIf<T>(this IEnumerable<T> query, bool condition, Func<T, bool> truePredicate, Func<T, bool> falsePredicate)
+        => condition ? query.Any(truePredicate) : query.Any(falsePredicate);
 
     public static bool SequenceEqual<T>(this IEnumerable<T> first, IEnumerable<T> second, IEqualityComparer<T> comparer)
         => first.SequenceEqual(second, comparer);
 
-    public static IEnumerable<T> DistinctByMany<T, TKey1, TKey2>(
-       this IEnumerable<T> source,
-       Func<T, TKey1> keySelector1,
-       Func<T, TKey2> keySelector2)
+    public static IEnumerable<T> DistinctByMany<T, TKey1, TKey2>(this IEnumerable<T> source, Func<T, TKey1> keySelector1, Func<T, TKey2> keySelector2)
     {
         var seenKeys = new HashSet<(TKey1, TKey2)>();
         return source.Where(item => seenKeys.Add((keySelector1(item), keySelector2(item))));
     }
 
-    public static IEnumerable<TResult> SelectManyWithIndex<TSource, TResult>(
-        this IEnumerable<TSource> source,
-        Func<TSource, int, TResult> selector)
-    {
-        return source.Select(selector);
-    }
+    public static IEnumerable<TResult> SelectManyWithIndex<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
+        => source.Select(selector);
 
     public static string ToDelimitedString<T>(this IEnumerable<T> source, string delimiter)
         => string.Join(delimiter, source);
-    
+
     public static string ToDelimitedString<T>(this IEnumerable<T> source, string delimiter, Func<T, string> selector)
-        => string.Join(delimiter, source.Select(selector)); 
-    
+        => string.Join(delimiter, source.Select(selector));
+
     public static bool None<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         => !source.Any(predicate);
 
@@ -113,4 +111,24 @@ public static class LinqExtensions
         random = new Random(seed);
         return source.Shuffle(random);
     }
+
+    public static T? FirstOrDefaultOr<T>(this IEnumerable<T> query, Func<T, bool> predicate, T? defaultValue = default)
+        => query.FirstOrDefault(predicate) ?? defaultValue;
+
+    public static string ToCsv<T>(this IEnumerable<T> query)
+    {
+        var headers = typeof(T).GetProperties().Select(p => p.Name);
+        var csv = new StringBuilder();
+        csv.AppendLine(string.Join(",", headers));
+        foreach (var entity in query)
+        {
+            if (entity == null) continue;
+            var values = headers.Select(h => entity.GetType().GetProperty(h)?.GetValue(entity)?.ToString() ?? string.Empty);
+            csv.AppendLine(string.Join(",", values));
+        }
+        return csv.ToString();
+    }
+
+    public static IEnumerable<T> RandomSample<T>(this IEnumerable<T> query, int sampleSize)
+        => query.OrderBy(_ => Guid.NewGuid()).Take(sampleSize);
 }
---
+++ src/everyextension/QueryableExtensions.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace EveryExtension;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        => condition ? query.Where(predicate) : query;
+
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition, Expression<Func<T, bool>> truePredicate, Expression<Func<T, bool>> falsePredicate)
+        => condition ? source.Where(truePredicate) : source.Where(falsePredicate);
+
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Func<T, int, bool> condition, Expression<Func<T, bool>> predicate)
+        => source.Where((item, index) => condition(item, index)).Where(predicate);
+
+    public static IQueryable<TResult> SelectIf<T, TResult>(this IQueryable<T> query, bool condition, Expression<Func<T, TResult>> selector)
+        => condition ? query.Select(selector) : query.Cast<T, TResult>();
+
+    public static IQueryable<TResult> SelectIf<T, TResult>(this IQueryable<T> query, bool condition, Expression<Func<T, TResult>> trueSelector, Expression<Func<T, TResult>> falseSelector)
+        => condition ? query.Select(trueSelector) : query.Select(falseSelector);
+
+    public static bool AnyIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        => condition ? query.Any(predicate) : true;
+
+    public static bool AnyIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> truePredicate, Expression<Func<T, bool>> falsePredicate)
+        => condition ? query.Any(truePredicate) : query.Any(falsePredicate);
+
+    public static IQueryable<T> OrderByDynamic<T>(this IQueryable<T> query, string propertyName, bool ascending)
+    {
+        var parameter = Expression.Parameter(typeof(T), "x");
+        var property = Expression.Property(parameter, propertyName);
+        var lambda = Expression.Lambda(property, parameter);
+        var methodName = ascending ? "OrderBy" : "OrderByDescending";
+        var methodCallExpression = Expression.Call(
+            typeof(Queryable),
+            methodName,
+            [typeof(T), property.Type],
+            query.Expression,
+            Expression.Quote(lambda));
+
+        return query.Provider.CreateQuery<T>(methodCallExpression);
+    }
+
+    public static IQueryable<T> Paginate<T>(this IQueryable<T> query, int page, int pageSize)
+        => query.Skip((page - 1) * pageSize).Take(pageSize);
+
+    private static IQueryable<TResult> Cast<T, TResult>(this IQueryable<T> query)
+        => query.Provider.CreateQuery<TResult>(query.Expression);
+
+    private static IQueryable<IGrouping<object, T>> GroupByMultipleKeys<T>(this IQueryable<T> query, params Expression<Func<T, object>>[] keySelectors)
+    {
+        var parameter = Expression.Parameter(typeof(T), "x");
+        var keyExpressions = keySelectors.Select(selector =>
+            Expression.Invoke(selector, parameter));
+        var keySelector = Expression.Lambda<Func<T, object[]>>(
+            Expression.NewArrayInit(typeof(object), keyExpressions),
+            parameter);
+        return query.GroupBy(keySelector);
+    }
+
+    public static T? FirstOrDefaultOr<T>(this IQueryable<T> query, Expression<Func<T, bool>> predicate, T? defaultValue = default)
+        => query.FirstOrDefault(predicate) ?? defaultValue;
+
+    public static string ToCsv<T>(this IQueryable<T> query)
+    {
+        var headers = typeof(T).GetProperties().Select(p => p.Name);
+        var csv = new StringBuilder();
+        csv.AppendLine(string.Join(",", headers));
+        foreach (var entity in query)
+        {
+            if (entity == null) continue;
+            var values = headers.Select(h => entity.GetType().GetProperty(h)?.GetValue(entity)?.ToString() ?? string.Empty);
+            csv.AppendLine(string.Join(",", values));
+        }
+        return csv.ToString();
+    }
+
+    public static IQueryable<T> RandomSample<T>(this IQueryable<T> query, int sampleSize)
+        => query.OrderBy(_ => Guid.NewGuid()).Take(sampleSize);
+
+    public static IQueryable<T> WhereNotNull<T, TValue>(this IQueryable<T> query, Expression<Func<T, TValue>> selector)
+        => query.Where(x => selector.Compile().Invoke(x) != null);
+}
