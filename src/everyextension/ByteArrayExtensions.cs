517126e70ebdb5a972ad649a597e6bbc30d32912
Dhurgham_Fahem <drm.fhm@gmail.com>
2024-02-12 21:17:14 +0300

    ByteArrayExtensions

--- src/everyextension/ByteArrayExtensions.cs
+++ src/everyextension/ByteArrayExtensions.cs
@@ -2,9 +2,20 @@
 
 public static class ByteArrayExtensions
 {
+    /// <summary>
+    /// Converts a byte array to a Guid.
+    /// </summary>
+    /// <param name="bytes">The byte array to convert.</param>
+    /// <returns>A Guid created from the byte array.</returns>
     public static Guid ToGuid(this byte[] bytes)
       => new(bytes);
 
+    /// <summary>
+    /// Converts a byte array to an array of Guids.
+    /// </summary>
+    /// <param name="byteArray">The byte array to convert.</param>
+    /// <returns>An array of Guids created from the byte array.</returns>
+    /// <exception cref="ArgumentException">Thrown if the byte array length is not a multiple of 16.</exception>
     public static Guid[] ToGuidArray(this byte[] byteArray)
     {
         if (byteArray.Length % 16 != 0)
