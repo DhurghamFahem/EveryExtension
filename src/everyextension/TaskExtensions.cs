00d3ad269f3ecff7870b05d6d34d32290ccda248
Dhurgham_Fahem <drm.fhm@gmail.com>
2024-02-12 22:20:54 +0300

    TaskExtensions

--- src/everyextension/TaskExtensions.cs
+++ src/everyextension/TaskExtensions.cs
@@ -1,7 +1,17 @@
 ﻿namespace EveryExtension;
 
+/// <summary>
+/// Provides extension methods for working with Tasks.
+/// </summary>
 public static class TaskExtensions
 {
+    /// <summary>
+    /// Adds a timeout to a Task, throwing a TimeoutException if the operation takes too long.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result produced by the task.</typeparam>
+    /// <param name="task">The Task to which a timeout is applied.</param>
+    /// <param name="timeout">The maximum duration allowed for the task to complete.</param>
+    /// <returns>A Task representing the original task with a timeout.</returns>
     public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout)
     {
         using var timeoutCancellationTokenSource = new CancellationTokenSource();
@@ -14,6 +24,13 @@ public static class TaskExtensions
         throw new TimeoutException("The operation has timed out.");
     }
 
+    /// <summary>
+    /// Adds a cancellation token to a Task, allowing for cancellation during execution.
+    /// </summary>
+    /// <typeparam name="T">The type of the result produced by the task.</typeparam>
+    /// <param name="task">The Task to which a cancellation token is added.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A Task representing the original task with a cancellation token.</returns>
     public static Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
     {
         return task.IsCompleted
@@ -26,6 +43,12 @@ public static class TaskExtensions
             );
     }
 
+    /// <summary>
+    /// Adds a cancellation token to a Task, allowing for cancellation during execution.
+    /// </summary>
+    /// <param name="task">The Task to which a cancellation token is added.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A Task representing the original task with a cancellation token.</returns>
     public static Task WithCancellation(this Task task, CancellationToken cancellationToken)
     {
         return task.IsCompleted
