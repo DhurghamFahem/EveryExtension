153de92ce57677ddee79cf6b04a20ab158a3f6c5
Dhurgham <drm.fhm@gmail.com>
2024-02-19 18:40:56 +0300

    starting with ExceptionExtensionsTests

---
+++ src/tests/ExceptionExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace EveryExtension.Tests;
+
+public class ExceptionExtensionsTests
+{
+    [Fact]
+    public void GetFullMessage_ReturnsCorrectResult()
+    {
+        // Arrange
+        var innerException1 = new Exception("Inner Exception 1");
+        var innerException2 = new Exception("Inner Exception 2", innerException1);
+        var exception = new Exception("Main Exception", innerException2);
+
+        // Act
+        string fullMessage = exception.GetFullMessage();
+
+        // Assert
+        Assert.Equal("Main Exception Inner Exception 2 Inner Exception 1", fullMessage);
+    }
+
+    [Fact]
+    public void GetFullMessage_ThrowsArgumentNullExceptionForNullException()
+    {
+        // Arrange & Act & Assert
+        Assert.Throws<ArgumentNullException>(() => ((Exception)null!).GetFullMessage());
+    }
+}
